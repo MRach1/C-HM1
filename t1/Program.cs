c8d359e30dd9b1b625faededbec7c0656e8a2300
Rach <rach_manukyan@mail.ru>
2022-11-29 22:44:17 +0300

    task 1

---
+++ t1/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = Console.ReadLine();
+int b = Console.ReadLine();
+int maxim = Math.Max(a, b);
+int minim = Math.Max(a, b);
+Console.WriteLine("max = ", maxim);
+Console.WriteLine("min = ", minim);
