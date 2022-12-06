501791a1c5a8188d47d5e8b9d77668b7e219f5f7
Rach <rach_manukyan@mail.ru>
2022-12-06 21:50:05 +0300

    дз 3

---
+++ dz3/t1/Program.cs
@@ -0,0 +1,29 @@
+﻿int numb = int.Parse(Console.ReadLine()!);
+int cln = numb;
+string simv = numb.ToString();
+int n = 0;
+int n0 = 0;
+while(cln > 0)
+{
+    cln = cln / 10;
+    n++;
+}
+n--;
+int F = 1;
+while(n0 < n)
+{
+    if (simv[n0] != simv[n])
+    {
+        F = 0;
+    }
+    n0++;
+    n--;
+}
+if (F == 1)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("НЕ палиндром");
+}
---
+++ dz3/t2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Координаты первой точки");
+Console.Write("X: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Y: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Z: ");
+int z1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Координаты второй точки");
+Console.Write("X: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.Write("Y: ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Z: ");
+int z2 = int.Parse(Console.ReadLine()!);
+
+double xlen = Math.Pow((x2 - x1), 2);
+double ylen = Math.Pow((y2 - y1), 2);
+double zlen = Math.Pow((z2 - z1), 2);
+double answ = Math.Sqrt(xlen + ylen + zlen);
+Console.WriteLine(Math.Round(answ, 2));
