7beba4afba1a395ce6abeeffc3ebb581f5e2c86c
Rach <rach_manukyan@mail.ru>
2022-12-10 23:31:40 +0300

    dz4 t2

--- dz4/t2/Program.cs
+++ dz4/t2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int fun(int a)
+{
+    int c = 0;
+    while (a > 0)
+    {
+        c = c + (a % 10);
+        a = a / 10;
+    }
+    return c;
+}
+int numb = int.Parse(Console.ReadLine()!);
+Console.WriteLine(fun(numb));
