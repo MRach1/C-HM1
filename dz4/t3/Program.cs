5a193c44622159e049f6ba6c4990fe31a75657a0
Rach <rach_manukyan@mail.ru>
2022-12-13 14:34:32 +0300

    finish dz4

--- dz4/t3/Program.cs
+++ dz4/t3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] mass = new int[8];
+Console.Write("Введите количество чисел в массиве (от 0 до 8): ");
+int n = int.Parse(Console.ReadLine()!);
+void fun(int a)
+{
+    int q = 0;
+    while(q < a)
+    {
+        Console.Write($"Введите {q + 1} число массива: ");
+        mass[q] = int.Parse(Console.ReadLine()!);
+        q++;
+    }
+    q = 0;
+    while (q < a)
+    {
+        Console.Write($"{mass[q]} ");
+        q++;
+    }
+}
+fun(n);
