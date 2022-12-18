3cb79c1965687c473022eb046b8e8e3d81b2c2b8
Rach <rach_manukyan@mail.ru>
2022-12-18 15:58:11 +0300

    dz6

---
+++ dz6/t1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Количество чисел: ");
+int M = int.Parse(Console.ReadLine()!);
+int count = 0;
+for(int i = 0; i < M; i++)
+{
+    Console.Write($"{i + 1} число: ");
+    int number = int.Parse(Console.ReadLine()!);
+    if (number > 0)
+    {
+        count++;
+    }
+}
+Console.Write(count);
