83c50e1a3677c22753c437c93f89a54a24d91ae8
Rach <rach_manukyan@mail.ru>
2022-12-30 20:50:49 +0300

    t123 1.1

---
+++ dz9/t2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("M = ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("N = ");
+int N = int.Parse(Console.ReadLine()!);
+int SumMN(int m, int n)
+{
+    int c = 0;
+    if(m <= n)
+    {
+        c = c + m + SumMN(m + 1, n);
+    }
+    return c;
+}
+Console.WriteLine(SumMN(M, N));
