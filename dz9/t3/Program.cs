5a75ead452b1adb7e5989311983193a1fd4c70b6
Rach <rach_manukyan@mail.ru>
2022-12-30 20:59:34 +0300

    t123 1.2

--- dz9/t3/Program.cs
+++ dz9/t3/Program.cs
@@ -26,31 +26,3 @@ int M = int.Parse(Console.ReadLine()!);
 Console.Write("N = ");
 int N = int.Parse(Console.ReadLine()!);
 Console.WriteLine(Ack(M, N));
-int Ack(int m, int n)
-{   
-    if (m >= 0 & n >= 0)
-    {
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    if (m > 0 & n == 0)
-    {
-        return Ack(m - 1, 1);
-    }
-    if(m > 0 & n > 0)
-    {
-        return Ack(m - 1, Ack(m, n -1));
-    }
-    }
-    else
-    {
-        return 0;
-    }
-    return(Ack(m, n));
-}
-Console.Write("M = ");
-int M = int.Parse(Console.ReadLine()!);
-Console.Write("N = ");
-int N = int.Parse(Console.ReadLine()!);
-Console.WriteLine(Ack(M, N));
